e04a49a8446673c3f33aea149008601c12029cb2
Nicolas BARBOTIN <nicolas@barbot.in>
2020-06-08 02:04:12 +0930

    * Shmem refinement

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,78 @@
+#![allow(dead_code)]
+
 #[cfg(not(feature = "expose-shmem"))]
 mod shmem;
 
 #[cfg(feature = "expose-shmem")]
 pub mod shmem;
 
-struct ExampleZone
-{
+struct ZoneDataTmp {
     uid: u32,
-    name: &'static str
+    color: shmem::Color,
+    start: shmem::Time,
+    duration: shmem::Duration,
+    name: &'static str,
+    copy_name: bool
 }
 
-const NUM_ZONES: usize = 4;
+impl shmem::WriteInto<shmem::ZoneData> for ZoneDataTmp {
+    fn write_into(&self, target: &mut shmem::ZoneData) {
+        target.uid = self.uid;
+        target.color = self.color;
+        target.start = self.start;
+        target.duration = self.duration;
+        target.name.set(self.name, self.copy_name);
+    }
+}
 
-const EXAMPLE_ZONES: [ExampleZone; NUM_ZONES] = [
-    ExampleZone { uid: 61, name: "Example zone 1" },
-    ExampleZone { uid: 62, name: "Example zone 2" },
-    ExampleZone { uid: 63, name: "Example zone 3" },
-    ExampleZone { uid: 64, name: "Example zone 4" }
-];
+#[cfg(test)]
+mod tests {
+    use rand::Rng as _;
+    use super::{shmem, ZoneDataTmp};
 
-use rand::Rng;
+    struct ExampleZone
+    {
+        uid: u32,
+        name: &'static str
+    }
 
-#[test]
-fn test_shmem() {
-    let mut mem = shmem::SharedMemory::open().expect("Failed to open shared memory. Make sure the server is actually running.");
-    let mut rng = rand::thread_rng();
-    let mut already_sent = [false; NUM_ZONES];
+    const NUM_ZONES: usize = 4;
 
-    for _ in 0..100 {
-        let chosen_zone = rng.gen_range(0, EXAMPLE_ZONES.len());
-        let ez = &EXAMPLE_ZONES[chosen_zone];
+    const EXAMPLE_ZONES: [ExampleZone; NUM_ZONES] = [
+        ExampleZone { uid: 61, name: "Example zone 1" },
+        ExampleZone { uid: 62, name: "Example zone 2" },
+        ExampleZone { uid: 63, name: "Example zone 3" },
+        ExampleZone { uid: 64, name: "Example zone 4" }
+    ];
 
-        let mut test: shmem::ZoneData = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
-        test.uid = ez.uid;
-        test.color = rng.gen();
-        test.start = rng.gen();
-        test.duration = rng.gen();
-        
-        unsafe {
-            test.name.set(ez.name, &mut already_sent[chosen_zone]);
-        }
+    #[test]
+    fn test_shmem() {
+        let mut mem = shmem::SharedMemory::open().expect("Failed to open shared memory. Make sure the server is actually running.");
+        let mut rng = rand::thread_rng();
+        let mut already_sent = [false; NUM_ZONES];
 
-        mem.zone_data.push(test);
+        for _ in 0..100 {
+            let chosen_zone = rng.gen_range(0, EXAMPLE_ZONES.len());
+            let ez = &EXAMPLE_ZONES[chosen_zone];
 
-        let pause = rng.gen_range(0, 100);
+            let test = ZoneDataTmp {
+                uid: ez.uid,
+                color: rng.gen(),
+                start: rng.gen(),
+                duration: rng.gen(),
+                name: ez.name,
+                copy_name: !already_sent[chosen_zone]
+            };
+            
+            if mem.zone_data.push(&test) {
+                already_sent[chosen_zone] = true;
+            }
 
-        if pause >= 5 {
-            std::thread::sleep(std::time::Duration::from_millis(pause));
+            let pause = rng.gen_range(0, 100);
+
+            if pause >= 5 {
+                std::thread::sleep(std::time::Duration::from_millis(pause));
+            }
         }
     }
 }
--- src/shmem.rs
+++ src/shmem.rs
@@ -10,11 +10,11 @@ use shared_memory::{Shmem, ShmemConf, ShmemError};
 pub const PROTOCOL_VERSION: u32 = 0x00_01_0000; //Major_Minor_Patch
 pub const NUM_ENTRIES: usize = 32;
 pub const LOG_DATA_SIZE: usize = 8192;
+pub const SHARED_STRING_MAX_SIZE: usize = 128;
 
 pub type Time = f64;     //TBD. Low precision time
 pub type Duration = u64; //TBD. High precision time difference
 pub type Color = u32;    //24 bits, 0x00RRGGBB
-pub type SmallRawString = [u8; 128];
 
 #[derive(Default)]
 struct SpinLock(AtomicBool);
@@ -41,34 +41,30 @@ impl SpinLock {
     }
 }
 
-#[derive(Copy, Clone, Default)]
+#[derive(Copy, Clone)]
 pub struct SharedString {
-    key: usize,                      //A number that uniquely identifies this zone's name string (typically, the string's address)
-    size: u8,                        //The length of this string, max 128 bytes
-    contents: Option<SmallRawString> //None if this string has already been sent. Otherwise, the string's contents
+    key: usize,                            //A number that uniquely identifies this zone's name string (typically, the string's address)
+    size: u8,                              //The length of this string, max 128 bytes
+    has_contents: bool,                    //False if this string has already been sent 
+    contents: [u8; SHARED_STRING_MAX_SIZE] //If has_contents is true, the string's contents
 }
 
 impl SharedString {
-    pub unsafe fn set(&mut self, string: &'static str, already_sent: &mut bool) {
+    pub fn set(&mut self, string: &str, copy_contents: bool) {
         let raw = string.as_bytes();
+        assert!(raw.len() <= SHARED_STRING_MAX_SIZE, "SharedStrings are limited to {} bytes", SHARED_STRING_MAX_SIZE);
+
         self.key = string.as_ptr() as usize;
         self.size = raw.len() as u8;
 
-        let opt_size = std::mem::size_of::<Option<SmallRawString>>() - std::mem::size_of::<SmallRawString>();
-        let content_ptr = (&mut self.contents as *mut Option<SmallRawString>) as *mut u8;
+        if copy_contents {
+            unsafe {
+                std::ptr::copy_nonoverlapping(raw.as_ptr(), self.contents.as_mut_ptr(), raw.len());
+            }
 
-        for i in 0..opt_size {
-            //Here we could use std::ptr::write_bytes but I suspect the compiler can optimize this better
-            //with a simple for loop as Option should be extremely small (opt_size is known at compile time)
-
-            *content_ptr.offset(i as isize) = 0;
-        }
-
-        if !*already_sent {
-            *content_ptr = 1; //This is terrible as it assumes little-endian and Option::Some == 1; but I don't know any better...
-
-            std::ptr::copy_nonoverlapping(raw.as_ptr(), self.contents.as_mut().unwrap().as_mut_ptr(), raw.len());
-            *already_sent = true;
+            self.has_contents = true;
+        } else {
+            self.has_contents = false;
         }
     }
 
@@ -79,12 +75,16 @@ impl SharedString {
 
     #[inline]
     pub fn make_string(&self) -> Option<String> {
-        self.contents.map(|c| unsafe { std::str::from_utf8_unchecked(&c[0..self.size as usize]).to_string() })
+        if self.has_contents {
+            Some(unsafe { std::str::from_utf8_unchecked(&self.contents[0..self.size as usize]).to_string() })
+        } else {
+            None
+        }
     }
 
     #[inline]
     pub fn has_contents(&self) -> bool {
-        self.contents.is_some()
+        self.has_contents
     }
 }
 
@@ -143,6 +143,16 @@ pub struct SharedMemoryData {
     pub log_data: [u8; LOG_DATA_SIZE] //Array of LogEntryHeader followed by `header.length` bytes of log message
 }
 
+pub trait WriteInto<T> {
+    fn write_into(&self, target: &mut T);
+}
+
+impl WriteInto<HeapData> for HeapData {
+    fn write_into(&self, target: &mut HeapData) {
+        *target = *self;
+    }
+}
+
 impl<T: Sized + Copy> Payload<T> {
     unsafe fn init(&mut self) {
         self.lock.unlock(); //Hack to init
@@ -150,15 +160,21 @@ impl<T: Sized + Copy> Payload<T> {
         
     }
 
-    pub fn push(&mut self, entry: T) {
+    pub fn push<U: WriteInto<T>>(&mut self, entry: &U) -> bool {
+        let ret;
         self.lock.lock();
 
         if self.size < NUM_ENTRIES {
-            self.data[self.size] = entry;
+            entry.write_into(&mut self.data[self.size]);
+            ret = true;
+        } else {
+            ret = false;
         }
 
         self.size += 1;
         self.lock.unlock();
+        
+        ret
     }
 
     pub unsafe fn retrieve_unchecked(&mut self, dst: *mut T) -> (usize, usize) {
@@ -205,6 +221,13 @@ pub struct SharedMemory {
     handle: Shmem
 }
 
+#[derive(Debug)]
+pub enum SharedMemoryOpenError {
+    ShmemError(ShmemError),
+    ProtocolMismatch,
+    PlatformMismatch
+}
+
 impl SharedMemory {
     pub fn get_path() -> PathBuf {
         let mut ret = data_dir().expect("could not find user data directory");
@@ -227,13 +250,22 @@ impl SharedMemory {
         Ok(SharedMemory { data, handle })
     }
 
-    pub fn open() -> Result<SharedMemory, ShmemError> {
+    pub fn open() -> Result<SharedMemory, SharedMemoryOpenError> {
         let handle = ShmemConf::new()
             .flink(Self::get_path().as_path())
-            .open()?;
+            .open().map_err(SharedMemoryOpenError::ShmemError)?;
 
         let data = handle.as_ptr() as *mut SharedMemoryData;
-        Ok(SharedMemory { data, handle })
+        let data_ref = unsafe { &mut *data };
+
+        if data_ref.protocol_version != PROTOCOL_VERSION {
+            Err(SharedMemoryOpenError::ProtocolMismatch)
+        } else if data_ref.size_of_usize != std::mem::size_of::<usize>() as u32 {
+            //Might happen if the lib was compiled for x86 and the server was compiled for x86_64
+            Err(SharedMemoryOpenError::PlatformMismatch)
+        } else {
+            Ok(SharedMemory { data, handle })
+        }
     }
 }
 
